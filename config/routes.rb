c28e22f52e64c4d69244a7d6c8f55c644fd61f54
eaesmat <ea.ahmadzai2020@gmail.com>
2022-11-22 15:05:26 +0430

    Setup project

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :accounts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: "public#homepage"
+end
